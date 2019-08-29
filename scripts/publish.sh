541e66d3b691d2d2a17078653b2abd7788626719
marcosschroh <schrohm@gmail.com>
2019-08-29 17:09:46 +0200

    fix: mkdocs links updated

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -11,7 +11,8 @@ twine upload dist/*
 
 # # creating git tag
 echo "Creating tag version v${VERSION}:"
-echo "git tag -a ${VERSION} -m 'Bump version v${VERSION}'"
+git tag -a v${VERSION} -m 'Bump version v${VERSION}'
+
 echo "git push origin v${VERSION}"
 
 # deploy documentation
