434576a791c966b232b19e277042fe907ca83d7d
marcosschroh <schrohm@gmail.com>
2019-08-31 12:31:25 +0200

    wip: many to many

--- tests/test_nested_schemas.py
+++ tests/test_nested_schemas.py
@@ -25,7 +25,7 @@ def test_one_to_one_relationship(user_one_address_schema):
 
 def test_one_to_many_relationship(user_many_address_schema):
     """
-    Test schema relationship one-to-one
+    Test schema relationship one-to-many
     """
     class Address:
         "An Address"
@@ -40,3 +40,22 @@ def test_one_to_many_relationship(user_many_address_schema):
 
     schema = SchemaGenerator(User).avro_schema()
     assert schema == json.dumps(user_many_address_schema)
+
+
+def test_one_to_many_map_relationship(user_many_map_address_schema):
+    """
+    Test schema relationship one-to-many using a map
+    """
+    class Address:
+        "An Address"
+        street: str
+        street_number: int
+
+    class User:
+        "User with multiple Address"
+        name: str
+        age: int
+        addresses: typing.Dict[str, Address]
+
+    schema = SchemaGenerator(User).avro_schema()
+    assert schema == json.dumps(user_many_map_address_schema)
