edbd0452835c1bf64984da189ec5bf8be4318fdf
aaron.fowles <aaron.fowles@ovoenergy.com>
2020-01-22 11:13:53 +0000

    add tests

---
+++ tests/fields/test_BaseField.py
@@ -0,0 +1,73 @@
+import dataclasses
+import typing
+
+from dataclasses_avroschema import fields
+
+
+def test_render():
+    field = fields.Field('first_name', str, fields.NULL, metadata={'desc': 'English Language Name'})
+
+    expected = {
+        "name": "first_name",
+        "type": [
+            "string",
+            fields.NULL
+        ],
+        "default": fields.NULL,
+        "desc": "English Language Name"
+    }
+
+    assert expected == field.render()
+
+    field = fields.Field('engine_name', str, fields.NULL)
+
+    expected = {
+        "name": "engine_name",
+        "type": [
+            "string",
+            fields.NULL
+        ],
+        "default": fields.NULL
+    }
+
+    assert expected == field.render()
+
+    field = fields.Field('breed_name', str, fields.NULL, metadata={'encoding': 'some_exotic_encoding', 'doc': 'Official Breed Name'})
+
+    expected = {
+        "name": "breed_name",
+        "type": [
+            "string",
+            fields.NULL
+        ],
+        "default": fields.NULL,
+        "encoding": "some_exotic_encoding",
+        "doc": "Official Breed Name",
+    }
+
+    assert expected == field.render()
+
+
+def test_render_metadata():
+    field = fields.Field('first_name', str, fields.NULL, metadata={'desc': 'English Language Name'})
+
+    expected = [
+        ("desc", "English Language Name")
+    ]
+
+    assert expected == field.render_metadata()
+
+    field = fields.Field('engine_name', str, fields.NULL)
+
+    expected = []
+
+    assert expected == field.render_metadata()
+
+    field = fields.Field('breed_name', str, fields.NULL, metadata={'encoding': 'some_exotic_encoding', 'doc': 'Official Breed Name'})
+
+    expected = [
+        ("encoding", "some_exotic_encoding"),
+        ("doc", "Official Breed Name")
+    ]
+
+    assert expected == field.render_metadata()
