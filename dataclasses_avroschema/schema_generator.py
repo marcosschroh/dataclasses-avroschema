f04b3c59ef6cd6925eddd1e7a073596d33a31f44
marcosschroh <schrohm@gmail.com>
2020-06-05 13:48:28 +0200

    wip return instance after deserialization

--- dataclasses_avroschema/schema_generator.py
+++ dataclasses_avroschema/schema_generator.py
@@ -1,6 +1,8 @@
 import dataclasses
 import json
+
 import typing
+from dacite import from_dict
 
 from dacite import Config, from_dict
 
@@ -89,7 +91,6 @@ class AvroModel:
     def deserialize(
         cls, data: bytes, serialization_type: str = AVRO, create_instance: bool = True
     ) -> typing.Union[typing.Dict, "AvroModel"]:
-
         schema = cls.avro_schema_to_python()
         payload = serialization.deserialize(data, schema, serialization_type=serialization_type)
 
