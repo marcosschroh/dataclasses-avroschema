7573fa54badb1afd966eec998ab8770f7d288055
Marcos Schroh <schrohm@gmail.com>
2020-07-15 20:34:44 +0200

    test: more test for serialization added (#60)

--- tests/serialization/test_nested_schema_serialization.py
+++ tests/serialization/test_nested_schema_serialization.py
@@ -1,70 +1,143 @@
-def test_complex_fields(user_advance_dataclass):
-    data = {
-        "name": "juan",
-        "age": 20,
-        "pets": ["dog"],
-        "accounts": {"ing": 100},
-        "has_car": True,
-        "favorite_colors": "GREEN",
-        "md5": b"u00ff",
+import dataclasses
+import typing
+
+from dataclasses_avroschema import AvroModel
+
+
+def test_one_to_one_relationship():
+    """
+    Test schema relationship one-to-one serialization
+    """
+
+    @dataclasses.dataclass
+    class Address(AvroModel):
+        "An Address"
+        street: str
+        street_number: int
+
+    @dataclasses.dataclass
+    class User(AvroModel):
+        "An User with Address"
+        name: str
+        age: int
+        address: Address
+
+    address_data = {
+        "street": "test",
+        "street_number": 10,
     }
 
-    expected_data = {
-        "name": "juan",
+    address = Address(**address_data)
+
+    data_user = {
+        "name": "john",
         "age": 20,
-        "pets": ["dog"],
-        "accounts": {"ing": 100},
-        "has_car": True,
-        "favorite_colors": "GREEN",
-        "md5": b"u00ff",
-        "country": "Argentina",
-        "address": None,
+        "address": address,
     }
 
-    data_json = {
-        "name": "juan",
-        "age": 20,
-        "pets": ["dog"],
-        "accounts": {"ing": 100},
-        "has_car": True,
-        "favorite_colors": "GREEN",
-        "md5": "u00ff",
-        "country": "Argentina",
-        "address": None,
+    user = User(**data_user)
+
+    avro_binary = b"\x08john(\x08test\x14"
+    avro_json_binary = b'{"name": "john", "age": 20, "address": {"street": "test", "street_number": 10}}'
+    expected = {"name": "john", "age": 20, "address": {"street": "test", "street_number": 10}}
+
+    assert user.serialize() == avro_binary
+    assert user.serialize(serialization_type="avro-json") == avro_json_binary
+    assert User.deserialize(avro_binary) == expected
+    assert User.deserialize(avro_json_binary, serialization_type="avro-json") == expected
+
+    assert user.to_json() == expected
+
+
+def test_one_to_many_relationship():
+    """
+    Test schema relationship one-to-many serialization
+    """
+
+    @dataclasses.dataclass
+    class Address(AvroModel):
+        "An Address"
+        street: str
+        street_number: int
+
+    @dataclasses.dataclass
+    class User(AvroModel):
+        "User with multiple Address"
+        name: str
+        age: int
+        addresses: typing.List[Address]
+
+    address_data = {
+        "street": "test",
+        "street_number": 10,
     }
 
-    user = user_advance_dataclass(**data)
+    address = Address(**address_data)
 
-    avro_binary = user.serialize()
-    avro_json = user.serialize(serialization_type="avro-json")
-
-    assert user_advance_dataclass.deserialize(avro_binary) == expected_data
-    assert user_advance_dataclass.deserialize(avro_json, serialization_type="avro-json") == expected_data
-    assert user.to_json() == data_json
-
-
-def test_complex_fields_with_defaults(user_advance_with_defaults_dataclass):
-    data = {
-        "name": "juan",
+    data_user = {
+        "name": "john",
         "age": 20,
+        "addresses": [address],
     }
 
-    expected_data = {
-        "name": "juan",
-        "age": 20,
-        "pets": ["dog", "cat"],
-        "accounts": {"key": 1},
-        "has_car": False,
-        "favorite_colors": "BLUE",
-        "country": "Argentina",
-        "address": None,
+    user = User(**data_user)
+
+    avro_binary = b"\x08john(\x02\x08test\x14\x00"
+    avro_json_binary = b'{"name": "john", "age": 20, "addresses": [{"street": "test", "street_number": 10}]}'
+    expected = {"name": "john", "age": 20, "addresses": [{"street": "test", "street_number": 10}]}
+
+    assert user.serialize() == avro_binary
+    assert user.serialize(serialization_type="avro-json") == avro_json_binary
+    assert User.deserialize(avro_binary) == expected
+    assert User.deserialize(avro_json_binary, serialization_type="avro-json") == expected
+
+    assert user.to_json() == expected
+
+
+def test_one_to_many_map_relationship():
+    """
+    Test schema relationship one-to-many using a map serialization
+    """
+
+    @dataclasses.dataclass
+    class Address(AvroModel):
+        "An Address"
+        street: str
+        street_number: int
+
+    @dataclasses.dataclass
+    class User(AvroModel):
+        "User with multiple Address"
+        name: str
+        age: int
+        addresses: typing.Dict[str, Address]
+
+    address_data = {
+        "street": "test",
+        "street_number": 10,
     }
 
-    user = user_advance_with_defaults_dataclass(**data)
+    address = Address(**address_data)
 
-    avro_binary = user.serialize()
-    avro_json = user.serialize(serialization_type="avro-json")
+    data_user = {
+        "name": "john",
+        "age": 20,
+        "addresses": {"main_residence": address},
+    }
 
-    assert user_advance_with_defaults_dataclass.deserialize(avro_binary) == expected_data
-    assert user_advance_with_defaults_dataclass.deserialize(avro_json, serialization_type="avro-json") == expected_data
-    assert user.to_json() == expected_data
+    user = User(**data_user)
+
+    avro_binary = b"\x08john(\x02\x1cmain_residence\x08test\x14\x00"
+    avro_json_binary = (
+        b'{"name": "john", "age": 20, "addresses": {"main_residence": {"street": "test", "street_number": 10}}}'
+    )
+    expected = {"name": "john", "age": 20, "addresses": {"main_residence": {"street": "test", "street_number": 10}}}
+
+    assert user.serialize() == avro_binary
+    assert user.serialize(serialization_type="avro-json") == avro_json_binary
+    assert User.deserialize(avro_binary) == expected
+
+    # seems that there is a bug in fastavro and raises KeyError
+    # assert User.deserialize(avro_json_binary, serialization_type="avro-json") == expected
+
+    assert user.to_json() == expected
