45ce7b2e291e7997fe2dc0370dc218a808856a79
Marcos Schroh <2828842+marcosschroh@users.noreply.github.com>
2022-04-05 18:40:36 +0200

    fix: alias for nested items (#169)
    Co-authored-by: Marcos Schroh <marcos.schroh@kpn.com>

--- tests/schemas/test_nested_schemas.py
+++ tests/schemas/test_nested_schemas.py
@@ -103,3 +103,39 @@ def test_alias_nested_item(user_one_address_alias_item):
             alias_nested_items = {"address": "Address"}
 
     assert User.avro_schema() == json.dumps(user_one_address_alias_item)
+
+
+def test_alias_nested_item_with_array(user_array_address_alias):
+    class Address(AvroModel):
+        "An Address"
+        street: str
+        street_number: int
+
+    class User(AvroModel):
+        "An User with Address"
+        name: str
+        age: int
+        address: typing.List[Address]  # default name Address
+
+        class Meta:
+            alias_nested_items = {"address": "MySuperAddress"}
+
+    assert User.avro_schema() == json.dumps(user_array_address_alias)
+
+
+def test_alias_nested_item_with_map(user_map_address_alias):
+    class Address(AvroModel):
+        "An Address"
+        street: str
+        street_number: int
+
+    class User(AvroModel):
+        "An User with Address"
+        name: str
+        age: int
+        address: typing.Dict[str, Address]  # default name Address
+
+        class Meta:
+            alias_nested_items = {"address": "MySuperAddress"}
+
+    assert User.avro_schema() == json.dumps(user_map_address_alias)
