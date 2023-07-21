5fe1634f10db62ccea928b1994f8ddcaa328a104
Michal Rogala <m.rogala@keelvar.com>
2023-07-21 12:22:20 +0000

    fix typing on Python 3.7

--- dataclasses_avroschema/fields/mapper.py
+++ dataclasses_avroschema/fields/mapper.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 import collections
 import datetime
 import decimal
@@ -8,7 +10,7 @@ from dataclasses_avroschema import types
 
 from . import fields, pydantic_fields
 
-INMUTABLE_FIELDS_CLASSES: dict[typing.Union[type, str], type] = {
+INMUTABLE_FIELDS_CLASSES: dict[type | str, type] = {
     bool: fields.BooleanField,
     int: fields.LongField,
     types.Int32: fields.IntField,
