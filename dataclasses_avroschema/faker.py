e64a048b695597662499a17f6afa0b50ec8dbed6
Aalekh Patel <aalekh@protectchildren.ca>
2023-08-25 11:11:46 -0500

    fix: declare FakeStub only in case of ImportError.

--- dataclasses_avroschema/faker.py
+++ dataclasses_avroschema/faker.py
@@ -1,46 +1,46 @@
-class FakeStub:
-    """A stub for Faker.fake() when the feature [faker] is not enabled.
-
-    It raises a hard RuntimeError when faker is not installed and
-    we tried to invoke `.fake()` on any of our fields.
-
-    We can find the local usages of faker methods (i.e. fake.<method>(*args, **kwargs)) with:
-
-    ```sh
-    rg "^.*fake\.(\w+)\(.*\).*$" dataclasses_avroschema -r '$1' -NIo | sort -u
-    ```
-    """
-
-    def raise_runtime_error(self, *args, **kwargs):
-        raise RuntimeError(
-            "faker must be installed in order to use .fake(). "
-            "Consider running `pip install dataclasses-avroschema[faker]`"
-        )
-
-
-_FAKER_METHODS_TO_STUB = [
-    "company_email",
-    "date_object",
-    "date_time",
-    "first_name",
-    "ipv4",
-    "last_name",
-    "pybool",
-    "pydecimal",
-    "pyfloat",
-    "pyint",
-    "pystr",
-    "time_object",
-    "uri",
-    "url",
-]
-
 try:
     from faker import Faker
 
     fake = Faker()
-
 except ImportError:
+
+    class FakeStub:
+        """A stub for Faker.fake() when the feature [faker] is not enabled.
+
+        It raises a hard RuntimeError when faker is not installed and
+        we tried to invoke `.fake()` on any of our fields.
+
+        We can find the local usages of faker methods (i.e. fake.<method>(*args, **kwargs)) with:
+
+        ```sh
+        rg "^.*fake\.(\w+)\(.*\).*$" dataclasses_avroschema -r '$1' -NIo | sort -u
+        ```
+        """
+
+        def raise_runtime_error(self, *args, **kwargs):
+            raise RuntimeError(
+                "faker must be installed in order to use .fake(). "
+                "Consider running `pip install dataclasses-avroschema[faker]`"
+            )
+
+
+    _FAKER_METHODS_TO_STUB = [
+        "company_email",
+        "date_object",
+        "date_time",
+        "first_name",
+        "ipv4",
+        "last_name",
+        "pybool",
+        "pydecimal",
+        "pyfloat",
+        "pyint",
+        "pystr",
+        "time_object",
+        "uri",
+        "url",
+    ]
+
     fake = FakeStub()
 
     for method in _FAKER_METHODS_TO_STUB:
