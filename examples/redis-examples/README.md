c67a9c3d588a3df8bed296dcb43e7f178770cdb2
marcosschroh <schrohm@gmail.com>
2024-02-13 13:47:28 +0100

    docs: examples refactored and docs updated.

---
+++ examples/redis-examples/README.md
@@ -0,0 +1,48 @@
+# Examples of dataclasses-avroschema
+
+You will find an example of how to use [dataclasses-avroschema](https://github.com/marcosschroh/dataclasses-avroschema) and the integration with the python drivers [walrus](https://github.com/coleifer/walrus)
+
+## Requirements
+
+`python 3.8+ poetry docker docker-compose`
+
+### Cluster setup
+
+1. `cd redis-examples`
+1. `make redis-cluster` to start the kafka cluster (kafka and zookeeper)
+
+## Examples
+
+As examples you will find how to serialize/deserialize python dataclasses using the `AvroModel` provided by `dataclasses-avroschema` and how to produce/consume events using specifics python kafka drivers.
+
+The model is for the examples is the following:
+
+```python
+class FavoriteColor(enum.Enum):
+    BLUE = "BLUE"
+    YELLOW = "YELLOW"
+    GREEN = "GREEN"
+
+
+@dataclass
+class UserModel(AvroModel):
+    "An User"
+    name: str
+    age: int
+    favorite_colors: FavoriteColor = FavoriteColor.BLUE
+    country: str = "Argentina"
+    address: Optional[str] = None
+
+    class Meta:
+        namespace = "User.v1"
+        aliases = ["user-v1", "super user"]
+```
+
+### dataclasses-avroschema and redis streams with walrus
+
+In the file [app.py](https://github.com/marcosschroh/dataclasses-avroschema/blob/master/examples/redis-examples/redis-streams-example/redis_streams_example/app.py) you will find a the simplest use case of `redis streams` using the driver `walrus`. We create a consumer group and messages are read one by one.
+
+1. `cd redis-examples/redis-streams-example`
+1. `poetry install`
+1. `poetry run app`
+1. To stop the producer/consumer use `Ctrl + c`
--- examples/redis-examples/redis-streams-example/redis_streams_example/app.py
+++ examples/redis-examples/redis-streams-example/redis_streams_example/app.py
@@ -1,29 +1,29 @@
-Minimal redis [example](https://github.com/marcosschroh/dataclasses-avroschema/tree/master/examples#dataclasses-avroschema-and-redis-streams-with-walrus) using `redis streams` with [walrus](https://github.com/coleifer/walrus) driver.
-
-```python title="Example with walrus driver"
-from dataclasses import dataclass
 import enum
 import random
+from dataclasses import dataclass
 from time import sleep
+from typing import Optional
 
 from walrus import Database  # A subclass of the redis-py Redis client.
 
-from dataclasses_avroschema import AvroModel, types
+from dataclasses_avroschema import AvroModel
 
 
 class FavoriteColor(enum.Enum):
-    BLUE = "Blue"
-    YELLOW = "Yellow"
-    GREEN = "Green"
+    BLUE = "BLUE"
+    YELLOW = "YELLOW"
+    GREEN = "GREEN"
+
 
 @dataclass
 class UserModel(AvroModel):
     "An User"
+
     name: str
     age: int
     favorite_colors: FavoriteColor = FavoriteColor.BLUE
     country: str = "Argentina"
-    address: str = None
+    address: Optional[str] = None
     testing: bool = False
 
     class Meta:
@@ -43,7 +43,7 @@ def consume(consumer_group):
             message_id, message_content = result[0]
 
             if message_id:
-                value = message_content[b'message']
+                value = message_content[b"message"]
                 print(f"Processing message {message_id} with value {value}")
                 user = UserModel.deserialize(value)
                 print(user)
@@ -53,8 +53,16 @@ def produce(consumer_group):
     for i in range(10):
         # create an instance of User v1
         user = UserModel(
-            name=random.choice(["Juan", "Peter", "Michael", "Moby", "Kim",]),
-            age=random.randint(1, 50)
+            name=random.choice(
+                [
+                    "Juan",
+                    "Peter",
+                    "Michael",
+                    "Moby",
+                    "Kim",
+                ]
+            ),
+            age=random.randint(1, 50),
         )
 
         msgid = consumer_group.my_stream.add({"message": user.serialize()})
@@ -65,18 +73,15 @@ def produce(consumer_group):
     sleep(2)
 
 
-if __name__ == "__main__":
+def main():
     db = Database()
-    stream_name = 'my-stream'
+    stream_name = "my-stream"
     db.Stream(stream_name)  # Create a new stream instance
 
     # create the consumer group
-    consumer_group = db.consumer_group('my-consumer-group-1', [stream_name])
+    consumer_group = db.consumer_group("my-consumer-group-1", [stream_name])
     consumer_group.create()  # Create the consumer group.
-    consumer_group.set_id('$')
+    consumer_group.set_id("$")
 
     produce(consumer_group)
     consume(consumer_group)
-```
-
-*(This script is complete, it should run "as is")*
