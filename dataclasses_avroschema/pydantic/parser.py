6df1b93136aae32554b66310cce5cfe36a627724
Kevin Jacobs <kevin.jacobs@delfidiagnostics.com>
2024-03-26 12:21:48 -0400

    Address PR comments   - added test case   - reverted changes to PydanticParser   - added PydanticParser.get_field_metadata staticmethod called from parse_fields() to obtain description as field doc   - added PydanticParser.generate_documentation to obtain title as record doc

--- dataclasses_avroschema/pydantic/parser.py
+++ dataclasses_avroschema/pydantic/parser.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 import dataclasses
 import typing
-from collections import OrderedDict
 
 from pydantic.fields import FieldInfo
 
@@ -11,32 +10,37 @@ from dataclasses_avroschema.fields.fields import AvroField
 from dataclasses_avroschema.parser import Parser
 
 
-def pydantic_to_avro_field(field_name: str, field_info: FieldInfo, parser: PydanticParser) -> Field:
-    metadata = field_info.json_schema_extra.get("metadata", {}) if field_info.json_schema_extra else {}
-    if field_info.description:
-        metadata["doc"] = field_info.description
-
-    return AvroField(
-        field_name,
-        field_info.rebuild_annotation(),
-        default=dataclasses.MISSING if field_info.is_required() or field_info.default_factory else field_info.default,
-        default_factory=field_info.default_factory,
-        metadata=metadata,
-        model_metadata=parser.metadata,
-        parent=parser.parent,
-    )
-
-
 class PydanticParser(Parser):
+    @staticmethod
+    def get_field_metadata(field_info: FieldInfo) -> dict[str, typing.Any]:
+        metadata = field_info.json_schema_extra.get("metadata", {}) if field_info.json_schema_extra else {}
+        if field_info.description:
+            metadata["doc"] = field_info.description
+        return metadata
+
     def parse_fields(self, exclude: typing.List) -> typing.List[Field]:
         return [
-            pydantic_to_avro_field(field_name, field_info, self)
+            AvroField(
+                field_name,
+                field_info.rebuild_annotation(),
+                default=dataclasses.MISSING
+                if field_info.is_required() or field_info.default_factory
+                else field_info.default,
+                default_factory=field_info.default_factory,
+                metadata=self.get_field_metadata(field_info),
+                model_metadata=self.metadata,
+                parent=self.parent,
+            )
             for field_name, field_info in self.type.model_fields.items()
             if field_name not in exclude and field_name != "model_config"
         ]
 
-    def render(self) -> OrderedDict:
-        schema = super().render()
-        if "doc" not in schema and self.type.model_config and "title" in self.type.model_config:
-            schema["doc"] = self.type.model_config["title"]
-        return schema
+    def generate_documentation(self) -> typing.Optional[str]:
+        doc = None
+        if isinstance(self.metadata.schema_doc, str):
+            doc = self.metadata.schema_doc
+        elif self.type.model_config and "title" in self.type.model_config:
+            doc = self.type.model_config["title"]
+        else:
+            doc = self.type.__doc__
+        return doc
